114c50d70b4e4bfb0d2501de247a0f19a494b0f8
DiegoH-cc <diegohugo824@gmail.com>
2024-08-31 18:23:34 -0300

    Update Tipos Variaveis

--- src/aulas/tiposVariaveis/TiposVariaveis.java
+++ src/aulas/tiposVariaveis/TiposVariaveis.java
@@ -2,4 +2,24 @@ package aulas.tiposVariaveis;
 
 public class TiposVariaveis {
     
+    public static void main(String[] args) throws Exception {
+        // tipos primtivos
+
+        String meuNome = "Meu Nome";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
 }
